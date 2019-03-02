9434716cf466ceb0e678a4e60c882d7f5104ffd9
Agnar <agnar-97@hotmail.com>
2019-03-02 22:18:22 +0000

    Revert "Merge branch 'master' of https://github.com/zeppelin-43/FlightSearchEngine"
    This reverts commit 72a6b55f8e66d523ae03d9e1667f0496e93406b8, reversing
    changes made to 0f0d2dd770783850c30f425d0b1c462392c62065.

--- FlightSearchEngine/src/is/hi/Main.java
+++ FlightSearchEngine/src/is/hi/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package is.hi.UI;
+package is.hi;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,13 +19,16 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
     }
+    //Hæ þetta ég...
+    
+    //Ég er bara þrullu góður
     /**
      * @param args the command line arguments
      */
